9c20640d9dc904a948f7e6addae3f6bf833fe99b
loreals2028 <loreal.smith2028@gbwl.org>
2026-09-18 16:35:04 +0000

    Wrote Notes

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 /*
 Comment Space
+Notes 9/18:
+Vocabulary:
+Algorithm: A step by step process to accomplish a task.
+ex: Morning routine, Cooking, School Schedule
+
+Pseudocode: Simplified code to outline programs/algorithms
+ex. add func()
+         num1
+         num2
+         1+2 prints (not actual syntax)
+
+Sequencing: the order of steps
+
+Question: What is the difference between Java Script and Java?
+
+Answer: Java is a programming language while JavaScript is a scripting language JavaScript resides inside HTML documents, and can provide levels of interactivity to web pages that are not achievable with simple HTML. Java is a versatile programming language, while JavaScript helps create interactive web pages. 
+
+JavaScript is more popular
+
+ In computer science, "programming language" is the umbrella term for any language used to give instructions to a computer. A scripting language is simply a specific subset of programming languages that is optimized for automation, rapid prototyping, and running inside a host environment without needing to be compiled first.
+
+
 */
 
 
